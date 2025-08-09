c7f7cbec6cedd7a6237b1aecc42e7b652d3b1fe6
pboyajian <peterhaigboyajian@gmail.com>
2025-08-09 14:22:54 -0400

    perf(MistakeList): add in-memory prep cache to avoid re-preparing when switching openings; reuse prepared items and patterns by stable key

--- server/types/index.d.ts
+++ server/types/index.d.ts
@@ -5,6 +5,7 @@ declare const compression: any;
 declare const Worker: any;
 declare const os: any;
 declare const app: any;
+declare const globalSummaryCache: any;
 declare const PORT: string | number;
 declare function analyzeWithParallelWorkers(games: any[], options?: any): Promise<any>;
 //# sourceMappingURL=index.d.ts.map
